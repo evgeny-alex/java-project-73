7759f160790e4b3a882bfe7f724f1634498cab7e
evgeny-alex <e.aleksandrov1@alumni.nsu.ru>
2022-10-29 15:17:20 +0700

    Added user model

---
+++ src/main/java/hexlet/code/app/model/User.java
@@ -0,0 +1,34 @@
+package hexlet.code.app.model;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+
+    @Id
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "firstName")
+    private String firstName;
+
+    @Column(name = "lastName")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "createdAt")
+    private Date createdAt;
+}
