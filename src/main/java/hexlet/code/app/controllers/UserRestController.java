d2508bd8b17e3ec536ca6e97e5ccac1c3d20b540
evgeny-alex <e.aleksandrov1@alumni.nsu.ru>
2022-11-01 11:33:10 +0700

    Описание методов CRUD пользователей + создание класса тестов

--- src/main/java/hexlet/code/app/controllers/UserRestController.java
+++ src/main/java/hexlet/code/app/controllers/UserRestController.java
@@ -1,7 +1,36 @@
 package hexlet.code.app.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import hexlet.code.app.dto.UserDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping("/users")
 public class UserRestController {
+
+    @PostMapping
+    public ResponseEntity<String> createUser(@RequestBody UserDto userDto) {
+        return ResponseEntity.ok("User successfully created");
+    }
+
+    @GetMapping
+    public ResponseEntity<UserDto> getUser(@PathVariable("id") String id) {
+        return ResponseEntity.ok(new UserDto());
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserDto>> getAllUser() {
+        return ResponseEntity.ok(List.of(new UserDto()));
+    }
+
+    @PutMapping
+    public ResponseEntity<String> updateUser(@RequestBody UserDto userDto, @PathVariable("id") String id) {
+        return ResponseEntity.ok("User successfully updated");
+    }
+
+    @DeleteMapping
+    public ResponseEntity<String> deleteUser(@PathVariable("id") String id) {
+        return ResponseEntity.ok("User successfully deleted");
+    }
 }
--- src/test/java/hexlet/code/app/controllers/UserRestControllerTest.java
+++ src/test/java/hexlet/code/app/controllers/UserRestControllerTest.java
@@ -1,4 +1,15 @@
 package hexlet.code.app.controllers;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class UserRestControllerTest {
+
+    // TODO: 01.11.2022 Дописать Unit тесты и дореализовать CRUD пользователей
+
+    @Test
+    public void getOneUserTest() {
+
+    }
 }
