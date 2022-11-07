c55fec436ca60e7d9703579ae6baf18272768c15
evgeny-alex <e.aleksandrov1@alumni.nsu.ru>
2022-11-07 09:11:22 +0300

    Добавлен authenticationManager.authenticate

--- src/main/java/hexlet/code/app/controllers/AuthRestController.java
+++ src/main/java/hexlet/code/app/controllers/AuthRestController.java
@@ -5,7 +5,10 @@ import hexlet.code.app.dto.UserRequestDto;
 import hexlet.code.app.model.User;
 import hexlet.code.app.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,9 +21,17 @@ public class AuthRestController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody LoginRequestDto loginRequestDto) {
-        // TODO: 04.11.2022 Доделать реализацию генерации токена
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequestDto.getEmail(), loginRequestDto.getPassword()));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("invalid username/password");
+        }
+        // TODO: 07.11.2022 Сгенерировать Jwt токен с помощью библиотеки
         return ResponseEntity.ok("");
     }
 
