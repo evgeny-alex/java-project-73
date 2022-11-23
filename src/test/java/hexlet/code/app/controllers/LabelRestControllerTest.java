dba20cc9998270646af3ab2772c5f0b79940dad0
evgeny-alex <e.aleksandrov1@alumni.nsu.ru>
2022-11-23 09:32:25 +0300

    Начало описания тестов для меток

---
+++ src/test/java/hexlet/code/app/controllers/LabelRestControllerTest.java
@@ -0,0 +1,35 @@
+package hexlet.code.app.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class LabelRestControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private ResourceLoader resourceLoader;
+
+    @Test
+    public void getOneLabelTest() throws Exception {
+        String labelId = "1";
+        mockMvc.perform(MockMvcRequestBuilders.get("/users/" + labelId))
+                .andExpect(status().isOk());
+    }
+
+    // TODO: 23.11.2022 Доделать тесты для всех контроллеров, замокать все обращения и проверить
+}
