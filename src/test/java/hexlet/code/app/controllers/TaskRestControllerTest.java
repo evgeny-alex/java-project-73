193842ef0b3a7c52c0067618af455d4bb0f17f3f
evgeny-alex <e.aleksandrov1@alumni.nsu.ru>
2023-01-15 11:23:24 +0300

    Промежуточное написание тестов для задач #2

--- src/test/java/hexlet/code/app/controllers/TaskRestControllerTest.java
+++ src/test/java/hexlet/code/app/controllers/TaskRestControllerTest.java
@@ -1,7 +1,9 @@
 package hexlet.code.app.controllers;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import hexlet.code.app.dto.*;
+import hexlet.code.app.model.Task;
 import hexlet.code.app.repository.LabelRepository;
 import hexlet.code.app.repository.TaskRepository;
 import hexlet.code.app.repository.TaskStatusRepository;
@@ -16,11 +18,18 @@ import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 import static hexlet.code.app.utils.TestUtils.baseUrl;
+import static hexlet.code.app.utils.TestUtils.fromJson;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@Transactional
 @SpringBootTest
 @AutoConfigureMockMvc
 public class TaskRestControllerTest {
@@ -94,7 +103,56 @@ public class TaskRestControllerTest {
     }
 
     @Test
-    void getOneTaskTest() {
-        // TODO: 13.01.2023 Доделать тест 
+    void getOneTaskTest() throws Exception {
+        Task task = taskRepository.findAll().get(0);
+
+        var response = mockMvc.perform(MockMvcRequestBuilders.get(baseUrl + "/tasks/" + task.getId()))
+                .andExpect(status().isOk()).andReturn().getResponse();
+
+        TaskResponseDto taskResponseDto = fromJson(response.getContentAsString(StandardCharsets.UTF_8), new TypeReference<>() {
+        });
+
+        assertEquals(task.getId(), taskResponseDto.getId());
+        assertEquals(task.getName(), taskResponseDto.getName());
+        assertEquals(task.getDescription(), taskResponseDto.getDescription());
+        assertEquals(task.getTaskStatus().getId(), taskResponseDto.getTaskStatus().getId());
+        assertEquals(task.getAuthor().getId(), taskResponseDto.getAuthor().getId());
+        assertEquals(task.getExecutor().getId(), taskResponseDto.getExecutor().getId());
+        assertEquals(task.getCreatedAt().getTime(), taskResponseDto.getCreatedAt().getTime());
     }
+
+    @Test
+    void getAllTaskTest() throws Exception {
+        long size = taskRepository.count();
+
+        var response = mockMvc.perform(MockMvcRequestBuilders.get(baseUrl + "/tasks")
+                        .header(AUTHORIZATION, token))
+                .andExpect(status().isOk()).andReturn().getResponse();
+
+        List<TaskResponseDto> taskResponseDtoList = fromJson(response.getContentAsString(StandardCharsets.UTF_8), new TypeReference<>() {
+        });
+
+        assertEquals(size, taskResponseDtoList.size());
+    }
+
+    @Test
+    void createTaskTest() throws Exception {
+        TaskRequestDto taskRequestDto = objectMapper.readValue(resourceLoader.getResource("classpath:json/request_create_task.json").getFile(), TaskRequestDto.class);
+        var response = mockMvc.perform(MockMvcRequestBuilders.post(baseUrl + "/tasks")
+                        .header(AUTHORIZATION, token)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(taskRequestDto))).andExpect(status().isOk())
+                .andReturn().getResponse();
+
+        TaskResponseDto taskResponseDto = fromJson(response.getContentAsString(StandardCharsets.UTF_8), new TypeReference<>() {
+        });
+
+        assertEquals(taskRequestDto.getName(), taskResponseDto.getName());
+        assertEquals(taskRequestDto.getDescription(), taskResponseDto.getDescription());
+        assertEquals(taskRequestDto.getTaskStatusId(), taskResponseDto.getTaskStatus().getId());
+        assertEquals(taskRequestDto.getAuthorId(), taskResponseDto.getAuthor().getId());
+        assertEquals(taskRequestDto.getExecutorId(), taskResponseDto.getExecutor().getId());
+    }
+
+    // TODO: 15.01.2023 Доделать остальные тесты
 }
