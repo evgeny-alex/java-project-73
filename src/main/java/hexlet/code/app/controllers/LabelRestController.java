0be1592b3c30515267174c288b963a9218ac080e
evgeny-alex <e.aleksandrov1@alumni.nsu.ru>
2023-01-22 12:46:43 +0300

    Описание документации для меток

--- src/main/java/hexlet/code/app/controllers/LabelRestController.java
+++ src/main/java/hexlet/code/app/controllers/LabelRestController.java
@@ -4,6 +4,9 @@ import hexlet.code.app.dto.LabelRequestDto;
 import hexlet.code.app.dto.LabelResponseDto;
 import hexlet.code.app.model.Label;
 import hexlet.code.app.services.LabelService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +21,11 @@ public class LabelRestController {
     @Autowired
     private LabelService labelService;
 
+    @Operation(summary = "Операция создания метки")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Метка успешно создана"),
+            @ApiResponse(responseCode = "500", description = "Произошла ошибка при создании метки")
+    })
     @PostMapping
     public ResponseEntity<LabelResponseDto> createLabel(@RequestBody LabelRequestDto labelDto) {
         Label label = labelService.createLabel(labelDto);
@@ -28,6 +36,11 @@ public class LabelRestController {
         return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
+    @Operation(summary = "Получение метки по идентификатору")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Метка возвращена"),
+            @ApiResponse(responseCode = "500", description = "Произошла ошибка при поиске метки")
+    })
     @GetMapping("/{id}")
     public ResponseEntity<LabelResponseDto> getLabel(@PathVariable("id") String id) {
         Label label = labelService.getLabelById(Integer.parseInt(id));
@@ -38,6 +51,11 @@ public class LabelRestController {
         return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
+    @Operation(summary = "Получение списка всех меток")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Список меток возвращен"),
+            @ApiResponse(responseCode = "500", description = "Произошла ошибка при поиске всех меток")
+    })
     @GetMapping
     public ResponseEntity<List<LabelResponseDto>> getAllLabel() {
         List<Label> labelList = labelService.getAllLabelList();
@@ -47,6 +65,11 @@ public class LabelRestController {
         return ResponseEntity.ok(labelResponseDtoList);
     }
 
+    @Operation(summary = "Обновление метки")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Метка успешно обновлена"),
+            @ApiResponse(responseCode = "500", description = "Произошла ошибка при обновлении метки")
+    })
     @PutMapping("/{id}")
     public ResponseEntity<LabelResponseDto> updateLabel(@RequestBody LabelRequestDto labelDto, @PathVariable("id") String id) {
         Label label = labelService.updateLabel(labelDto, Integer.parseInt(id));
@@ -57,6 +80,11 @@ public class LabelRestController {
         return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
+    @Operation(summary = "Удаление метки")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Метка успешно удалена"),
+            @ApiResponse(responseCode = "500", description = "Произошла ошибка при удалении метки")
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteLabel(@PathVariable("id") String id) {
         labelService.deleteLabel(Integer.parseInt(id));
